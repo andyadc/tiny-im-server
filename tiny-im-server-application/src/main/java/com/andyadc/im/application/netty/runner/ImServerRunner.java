f68bae06ff376850443f69ab9343102aaf4326d8
andyadc <andyadc@foxmail.com>
2023-12-21 16:14:44 +0800

    im server

---
+++ tiny-im-server-application/src/main/java/com/andyadc/im/application/netty/runner/ImServerRunner.java
@@ -0,0 +1,13 @@
+package com.andyadc.im.application.netty.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImServerRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ tiny-im-server-starter/src/main/java/com/andyadc/im/ImServerStarter.java
@@ -0,0 +1,12 @@
+package com.andyadc.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImServerStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImServerStarter.class, args);
+    }
+}
