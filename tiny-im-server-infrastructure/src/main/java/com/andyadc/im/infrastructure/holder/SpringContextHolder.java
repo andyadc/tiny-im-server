0d60c9aa0f5d3d6e061ba3d4cad795ac6e84acce
andyadc <andyadc@foxmail.com>
2023-12-21 18:05:41 +0800

    im server

---
+++ tiny-im-server-infrastructure/src/main/java/com/andyadc/im/infrastructure/holder/SpringContextHolder.java
@@ -0,0 +1,17 @@
+package com.andyadc.im.infrastructure.holder;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextHolder implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextHolder.applicationContext = applicationContext;
+    }
+}
